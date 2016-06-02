7f741b8d46bb6c24e23d6a476c26ac5e297fdfa5
Krys <aslay.k@qq.com>
2016-06-02 10:55:05 +0000

    singleton

---
+++ krysnet/base/singleton.hpp
@@ -0,0 +1,27 @@
+#ifndef _SINGLETON_
+#define _SINGLETON_
+
+namespace krys
+<%
+
+
+template<typename T>
+class singleton
+{
+public:
+	static T& instance ()
+	{
+		static T obj_instance;
+		return obj_instance;
+	}
+	singleton () = delete;
+	singleton (const singleton&) = delete;
+	singleton (singleton&&) = delete;
+
+private:
+};
+
+
+%>
+
+#endif
